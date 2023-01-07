c6fa246b30daf36e45efcbc39baa5fe4d11de499
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2023-01-07 08:16:58 +0500

    fmp website

---
+++ public/app.js
@@ -0,0 +1,8 @@
+function abc(para) {
+    para.src = ("https://theoriginalguidetomenshealth.org/wp-content/uploads/2019/12/naturopathic-opt.jpg")
+}
+
+function xyz(para) {
+    para.src = ("https://tse3.mm.bing.net/th?id=OIP.V_hRF4u-Gp3LXAZ8dfRdsgEsDh&pid=Api&P=0")
+}
+
